e370a5097ba5749e04166ab24b617c3aac442fed
caio-pixel <silva.garcia@aluno.ifsp.edu.br>
2025-04-29 08:57:59 -0300

    exemplos: contador + imagem

--- etapa2/app-react-exemolos/App.js
+++ etapa2/app-react-exemolos/App.js
@@ -1,14 +1,37 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(counter + 1)
+  };
+  const decrementCounter = () => {
+    setCounter(counter - 1)
+  };
   return (
     <View style={styles.container}>
-      <Text>ola app react native!</Text>
+      <Text style={styles.text}>ola app react native!</Text>
+      <Image 
+        soure={{url: "https://picsum.photo/200"}}
+        style={{widht: 200, height: 200}}
+      />
+
       <StatusBar style="auto" />
+      <Text style={(styles.text)}>Counter: {counter}</Text>
+
+      <View style={styles.buttonContainer}>
+        <Button title='Increment' onPress={incrementCounter}/>
+        <Button title='descrement' onPress={decrementCounter} />
+      </View>
     </View>
   );
 }
+
+
 
 const styles = StyleSheet.create({
   container: {
@@ -17,4 +40,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 24,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+  }
+ 
 });
