e40531a49875c1875ecd185b9c2faaad95db3178
caio-pixel <silva.garcia@aluno.ifsp.edu.br>
2025-05-13 09:17:12 -0300

    "TextInput basic examples"

--- etapa2/app-react-exemolos/App2.js
+++ etapa2/app-react-exemolos/App2.js
@@ -1,16 +1,18 @@
 import React, { useState } from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from 'react-native';
+import List from "./components/list";
 
-
-import Inputs from "./components/inputs";
 
 export default function App() {
 
 
   return (
     <View style={styles.container}>
-     <Inputs/>
+      <List />
+      <View style={styles.redbox}></View>
+      <View style={styles.bluebox}></View>
+      <View style={styles.blackbox}></View>
     </View>
 
 
@@ -25,5 +27,21 @@ const styles = StyleSheet.create({
     backgroundColor: 'gray',
     height: 600,
     marginTop: 150,
+  },
+  redbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+  },
+  bluebox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    borderRadius: 20,
+  },
+  blackbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
   }
 });
