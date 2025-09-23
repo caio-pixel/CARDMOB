83a3a193b10f074b1e2cfb409a6dab64d5f2a318
Caio garcia <caiogarcia26s@gmail.com>
2025-09-23 10:18:32 -0300

    Carrinho de compras salvado o estado dos items

--- etapa3/loja-react-app/App.tsx
+++ etapa3/loja-react-app/App.tsx
@@ -2,15 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import { ThemeProvider } from './src/context/ThemeContext'; // NOVA
-import HomeScreen from './src/screens/HomeScreen';
+// import HomeScreen from './src/screens/HomeScreen';
 import RootNavigator from './src/navigation/RootNavigator';
 import { AuthProvider } from './src/context/AuthContext';
+import { ShopProvider } from './src/context/ShopContext';
+
 
 export default function App() {
   return (
     <ThemeProvider>
       <AuthProvider>
-      <RootNavigator />
+        <ShopProvider>
+          <RootNavigator />
+        </ShopProvider>
       </AuthProvider>
     </ThemeProvider>
   );
