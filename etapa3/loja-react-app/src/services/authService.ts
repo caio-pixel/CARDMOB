265aaa902d201ef9e1bd7e4e468db2ccacf17df0
Caio garcia <caiogarcia26s@gmail.com>
2025-11-18 09:59:12 -0300

    Extrai dados do usuário do token

--- etapa3/loja-react-app/src/services/authService.ts
+++ etapa3/loja-react-app/src/services/authService.ts
@@ -33,3 +33,40 @@ export async function requestLogin(email: string, password: string): Promise<str
         // return Promise.reject('Credenciais inválidas');
     }
 }
+
+// dupliquei o requestLogin, renomeie e inclui o name nos argumentos.
+export async function requestRegister(name: string, email: string, password: string): Promise<string> {
+    try {
+        const response = await fetch(`${apiUrl}/api/users`, { // modifiquei o endpoint
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({name, email, password}), // inclui o name
+        });
+        const data = await response.json();
+        const jwt = data.accessToken;
+        console.log(jwt);
+        return Promise.resolve(jwt);
+    }
+    catch (error) {
+        console.error(error);
+        return Promise.reject(error);
+        // return Promise.reject('Credenciais inválidas');
+    }
+}
+
+export async function getTokenData(token: string | null): Promise<any[]> {
+    try {
+        if (!token) {
+            return [];
+        }
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const payload = JSON.parse(atob(base64));
+        return payload;
+    } catch (error) {
+        console.error(error);
+        return Promise.reject('Token inválido.');
+    }
+}
